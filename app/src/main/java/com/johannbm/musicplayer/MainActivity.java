0958980f334b3b869dc7dbdeb0d58042f3229bf6
Roco <jbmaschio@gmail.com>
2019-02-12 15:12:26 -0200

    coments

--- app/src/main/java/com/johannbm/musicplayer/MainActivity.java
+++ app/src/main/java/com/johannbm/musicplayer/MainActivity.java
@@ -64,6 +64,7 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
         });
     }
 
+    // Set up from layout 
     public void setUpUI(){
 
         mediaPlayer = new MediaPlayer();
